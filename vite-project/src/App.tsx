4db4786b99e4773162321c70b8a77dad6f8617dc
vinh15cm <vinhnguyen121105@gmail.com>
2024-05-27 22:21:59 +0700

    add session41BT

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './index.css'
+import B1 from './components/B1'
+import B2 from './components/B2'
+import B3 from './components/B3'
+import B4 from './components/B4'
+import B5 from './components/B5'
+import B6 from './components/B6'
+export default function App() {
+  return (
+    <div>
+      <B1></B1>
+      <B2></B2>
+      <B3></B3>
+      <B4></B4>
+      <B5></B5>
+      <B6></B6>
+    </div>
+  )
+}
---
+++ vite-project/src/components/B1.tsx
@@ -0,0 +1,8 @@
+export default function B1() {
+    return (
+        <div>
+            B1
+            <h1 className="text-blue-500 text-[32px]">Đây là tiêu đề h1</h1>
+        </div>
+    );
+}
